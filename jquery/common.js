b851f4b0a8b1b48efb6785be038da88a0ffd05bb
minsssss00 <minsun1028@gmail.com>
2023-02-06 13:49:37 +0900

    sub1
    header 수정

--- jquery/common.js
+++ jquery/common.js
@@ -1,9 +1,11 @@
 
+const Bug = document.querySelector('.')
+
 const Cl = document.querySelector('.popmenu .closeBtn');
 const Menu = document.querySelector('.popmenu');
 console.log(Cl);
 
 Cl.addEventListener('click', function () {
-  Menu.style = "display:none"
+    Menu.style = "display:none;"
 });
 
