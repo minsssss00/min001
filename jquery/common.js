fe1969c311994ca771ab6a9a385c1b3c64c02973
minsssss00 <minsun1028@gmail.com>
2023-02-06 10:04:54 +0900

    login

--- jquery/common.js
+++ jquery/common.js
@@ -1,6 +1,7 @@
 const Cl = document.querySelector('.popmenu .closeBtn');
 const Menu =  document.querySelector('.popmenu') ;
 console.log(Cl);
+
 Cl.addEventListener('click',function(){
     Menu.style = "display:none"
   });
